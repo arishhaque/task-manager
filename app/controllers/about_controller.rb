6a09ffdb96053ac70da7da50d2bc841577890865
arishhaque <arish.haq@gmail.com>
2023-04-29 16:45:22 -0700

    added about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    @created_by = "Arish"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  get 'home/index', to: 'task#index'
+  get 'about', to: 'about#index'
 end
