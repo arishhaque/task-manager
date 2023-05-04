38682d62feb4ff3e676a16359da59c9dfd6fadf7
arishhaque <arish.haq@gmail.com>
2023-05-03 21:50:51 -0700

    good cm: tasks completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,49 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order(:name)
   end
 
-  def show; end
+  def show
+    @task = Task.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @task = Task.new
+  end
 
-  def create; end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render('new')
+    end
+  end
 
   def edit
     @task = Task.find(params[:id])
   end
 
-  def update; end
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render('edit')
+    end
+  end
 
   def delete; end
 
   def destroy; end
+
+  private
+
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :status,
+      :description
+    )
+  end
 end
