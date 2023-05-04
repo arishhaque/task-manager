9bcfe9f1d5e0076a69bd37ff3ae0930115a16fce
arishhaque <arish.haq@gmail.com>
2023-05-03 20:36:14 -0700

    added task and category

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,28 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
-  get 'home/index', to: 'task#index'
+  get 'home/index', to: 'tasks#index'
   get 'about', to: 'about#index'
 
   # Tasks routes
-  get 'tasks', to: 'task#index'
-  get 'tasks/new', to: 'task#new'
-  get 'tasks/edit/:id', to: 'task#edit'
-
+  resources :tasks do
+    member do
+      get :delete
+    end
+  end
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # get 'tasks/:id/delete', to: 'tasks#edit'
+  resources :categories do
+    member do
+      get :delete
+    end
+  end
   # Category routes
-  get 'categories/', to: 'category#index'
-  get 'categories/show', to: 'category#show'
-  get 'categories/new', to: 'category#new'
-  get 'categories/edit/:id', to: 'category#edit'
-  get 'categories/delete', to: 'category#delete'
+  # get 'categories/', to: 'categories#index'
+  # get 'categories/show', to: 'categories#show'
+  # get 'categories/new', to: 'categories#new'
+  # get 'categories/:id/edit', to: 'categories#edit'
+  # get 'categories/:id/delete', to: 'categories#delete'
 end
