a84a32b439c4be9e5cad83830ed1dd9bc0c0b3fe
arishhaque <arish.haq@gmail.com>
2023-05-06 11:18:16 -0700

    completed tasks and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,47 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.order(:name)
+  end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
 
-  def create; end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render('new')
+    end
+  end
 
-  def edit; end
+  def edit
+    @category = Category.find(params[:id])
+  end
 
-  def update; end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render('edit')
+    end
+  end
 
   def delete; end
 
   def destroy; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 end
